7e21cf50c6e3f405f80172c2feae5e6e1292dbc3
Akash <akashjan4@gmail.com>
2018-11-14 17:14:49 +0530

    implemented ngIF,ngFor,ngClass directives

--- ng7/src/app/server/server.component.ts
+++ ng7/src/app/server/server.component.ts
@@ -1,18 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IMyServerList {
+    name: string;
+    location: string;
+    ip: string;
+    port: number;
+}
+
 @Component({
     selector: 'app-server', // as element
-   // selector: '.app-server', -> as class
-   // selector: ['app-server'], -> as attribute
+    // selector: '.app-server', -> as class
+    // selector: ['app-server'], -> as attribute
     templateUrl: './server.component.html',
     styleUrls: ['./server.scss']
 })
 
-export class ServerComponent implements OnInit{
+export class ServerComponent implements OnInit, IMyServerList {
     serverIp = '127.22.12.1';
     serverStatus = true;
+    serverList: IMyServerList[];
+    name: string;
+    location: string;
+    ip: string;
+    port: number;
 
     ngOnInit() {
+        this.serverList = [
+            {
+                name: 'Fk-4554-001',
+                location: 'Florida',
+                ip: '127.40.234.1',
+                port: 2365,
+            },
+            {
+                name: 'IG-8454-242',
+                location: 'London',
+                ip: '127.90.24.111',
+                port: 1165,
+            },
+            {
+                name: 'LP-4421-211',
+                location: 'Jhuan',
+                ip: '112.40.12.167',
+                port: 2981,
+            },
+            {
+                name: 'JK-234-871',
+                location: 'Pune',
+                ip: '141.87.39.229',
+                port: 8428,
+            }
+        ];
     }
 
     isServerLive() {
@@ -20,6 +58,6 @@ export class ServerComponent implements OnInit{
     }
 
     switchServerStatus() {
-        this.serverStatus = !this.serverStatus;
+        this.serverStatus = Math.random() > 0.5 ? true : false;
     }
 }
