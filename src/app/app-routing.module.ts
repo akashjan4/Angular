dd534d16400feac0cb5a21762782b66ee2da6fa2
Akash <akashjan4@gmail.com>
2021-03-06 02:38:10 +0530

    added new header, body, footer component added css for header,body,footer setup style foler with mobile &  tablet updated style path on angular.json added new page components updated router added content-projection on body component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { TrendingComponent } from './trending/trending.component';
+import { WatchListComponent } from './watch-list/watch-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: "full", redirectTo:'/watch-list'
+  },
+  {
+    path: 'watch-list', component: WatchListComponent,
+  },
+  {
+    path: 'trending', component: TrendingComponent,
+  },
+  {
+    path: 'about', component: AboutComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { WatchListComponent } from './watch-list/watch-list.component';
+import { TrendingComponent } from './trending/trending.component';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    FooterComponent,
+    HeaderComponent,
+    WatchListComponent,
+    TrendingComponent,
+    SearchComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
